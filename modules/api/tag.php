addac5d8bc14c7cfd5e0d43d54b9c1cbd8482ccf
Nolan Perry <github@nolanperry.rocks>
2019-10-24 19:59:30 -0400

    Update tag.php

--- modules/api/tag.php
+++ modules/api/tag.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 header('Content-type: text/html; charset=utf-8');
 include("config.php");
 mysqli_set_charset($db, "utf8");
