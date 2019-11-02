d25e99d274341727e0ac824f02c758fe5b80f8c9
Nolan Perry <github@nolanperry.rocks>
2019-11-02 19:39:02 -0400

    Update region.php

--- modules/api/region.php
+++ modules/api/region.php
@@ -9,11 +9,11 @@ require( "config.php" );
 $region = $_REQUEST['region'];
 $count = $_REQUEST['count'];
 // SQL statments
-$findit = "SELECT * FROM `visitors` WHERE `name` LIKE '" . $region . "'";
+$findit = "SELECT * FROM `visitors` WHERE `name` LIKE '" . $region . "';";
 $result = mysqli_query( $db,$findit );
- $update = "UPDATE `visitors` SET `count` = '" . $count . "' WHERE `visitors`.`region ` = '". $region . "'";
+ $update = "UPDATE `visitors` SET `count` = '" . $count . "' WHERE `visitors`.`region ` = '". $region . "';";
 $isRecord = mysqli_num_rows($result);
-   $add = "INSERT INTO `visitors` (, `name`, `count`) VALUES ('" . $region . "','" . $count . "')";
+   $add = "INSERT INTO `visitors` (, `name`, `count`) VALUES ('" . $region . "','" . $count . "');";
 if ( $isRecord == 0 ) // Is there a record already?
 {  
 $hi =  mysqli_query($db,$add);
