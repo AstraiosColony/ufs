b65eae30c621fc67e6eb36761b1819f5958145dc
Nolan Perry <github@nolanperry.rocks>
2019-11-23 17:08:06 -0500

    Update tag.php

--- modules/api/tag.php
+++ modules/api/tag.php
@@ -6,7 +6,7 @@ mysqli_set_charset($db, "utf8");
 //    ini_set('default_charset', 'UTF-8');
 $uuid  = $_REQUEST['uuid'];
 $error = "\n\nFailed to update Group Tag.\nContact Astraios Colony Engineering with the provided Error Message\n";
-$Tag   = "SELECT IFNULL(a.`DisplayName`, a.`username`) AS `name`, a.`active`, r.`rname`, t.`tag_name`, d.`colorX`, d.`colorY`, d.`ColorZ`, r.`RankLogo`, `Position` FROM `accounts` a INNER JOIN `divisions` d ON a.`DivID` = d.`did` INNER JOIN `rank` r ON a.`RankID` = r.`RankID` INNER JOIN `titles` t ON a.`TitleID` = t.`tid` WHERE `UUID` = '$uuid' LIMIT 1";
+$Tag   = "SELECT IFNULL(a.`DisplayName`, a.`username`) AS `name`, a.`active`, r.`rname`, t.`tag_name`, d.`colorX`, d.`colorY`, d.`ColorZ`, r.`RankLogo`, `Position`, d.`dname` FROM `accounts` a INNER JOIN `divisions` d ON a.`DivID` = d.`did` INNER JOIN `rank` r ON a.`RankID` = r.`RankID` INNER JOIN `titles` t ON a.`TitleID` = t.`tid` WHERE `UUID` = '$uuid' LIMIT 1";
 $query = mysqli_query($db, $Tag);
 $Rows  = mysqli_num_rows($query);
 if ($Rows == 0) // Is there a record already?
