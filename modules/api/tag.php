7463f1c694352cc3ddc76d5f7831f34b3505a83f
Nolan Perry <github@nolanperry.rocks>
2019-11-23 17:28:24 -0500

    Update tag.php

--- modules/api/tag.php
+++ modules/api/tag.php
@@ -6,7 +6,7 @@ mysqli_set_charset($db, "utf8");
 //    ini_set('default_charset', 'UTF-8');
 $uuid  = $_REQUEST['uuid'];
 $error = "\n\nFailed to update Group Tag.\nContact Astraios Colony Engineering with the provided Error Message\n";
-$Tag   = "SELECT IFNULL(a.`DisplayName`, a.`username`) AS `name`, a.`active`, r.`rname`, t.`tag_name`, d.`colorX`, d.`colorY`, d.`ColorZ`, r.`RankLogo`, `Position`, d.`dname` FROM `accounts` a INNER JOIN `divisions` d ON a.`DivID` = d.`did` INNER JOIN `rank` r ON a.`RankID` = r.`RankID` INNER JOIN `titles` t ON a.`TitleID` = t.`tid` WHERE `UUID` = '$uuid' LIMIT 1";
+$Tag   = "SELECT IFNULL(a.`DisplayName`, a.`username`) AS `name`, a.`active`,a.`species`, r.`rname`, t.`tag_name`, d.`colorX`, d.`colorY`, d.`ColorZ`, r.`RankLogo`, `Position`, d.`dname` FROM `accounts` a INNER JOIN `divisions` d ON a.`DivID` = d.`did` INNER JOIN `rank` r ON a.`RankID` = r.`RankID` INNER JOIN `titles` t ON a.`TitleID` = t.`tid` WHERE `UUID` = '$uuid' LIMIT 1";
 $query = mysqli_query($db, $Tag);
 $Rows  = mysqli_num_rows($query);
 if ($Rows == 0) // Is there a record already?
@@ -24,12 +24,16 @@ if ($Rows == 0) // Is there a record already?
     $logo     = $list['RankLogo'];
     $station  = $list['Position'];
     $division = $list['dname'];
+    $species = $list['species'];
     if (0 == $list['active']) {
         echo "<255,255,255>:" . $logo . "\nCivilian\nUFS";
     } else {
         // In the line by line examples, a * represents new line
         //   echo "<".$colorX.",".$colorY.",".$colorZ.">:".$logo."\n".$rank."\n".$name."\n".$tag."\n".$station // This shows Logo*Rank*Name*Position*Current Station
         echo "<" . $colorX . "," . $colorY . "," . $colorZ . ">:" . $name . " \n " . $rank . " | " .  $division . " \n " . $tag . " | " . $station;
+        if($species !== "Human"){
+            echo "\n ". $species;
+        }
     }
 }
 ?>
